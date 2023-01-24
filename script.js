be5bf60a0ef96806023b87ef869e3ea87b57a4e8
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-01-24 14:02:15 +0530

    done

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-let reversedStr = str.split('').reverse().join('');
-    return str === reversedStr;
-	
+let a = str.toLowerCase();
+
+let ans = "";
+for(let i=a.length;i>=0;i--){
+	ans+=a.charAt(i);
+}
+if(a === ans){
+    return true;
+}
+return false;
 }
 module.exports = palindrome
